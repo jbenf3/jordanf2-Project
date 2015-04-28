dfd1e624e2a844eaee43c0f18c990169c03dbc58
Jordan Friedland <jordanf2@stanford.edu>
2015-04-27 22:41:07 -0700

    submitting milestone4

---
+++ milestone4/test.py
@@ -0,0 +1,10 @@
+# The functionality for the program is fairly minimal because I am still
+# in the process of implementing the scoring and statistics tracking
+# mechanism, but I have cleaned up all of the errors that were in the code
+# and although a couple functions are commented out, it so far is doing
+# what I would like it to do which is to create two player objects and
+# then ask for their names. It will be relatively simple to implement the
+# scoring mechanism that I have chosen, but if in the future I have more
+# time to increase the complexity of the scoring mechanism, I will do so.
+
+execfile("projectredux.py")
